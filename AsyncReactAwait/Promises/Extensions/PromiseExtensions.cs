03c392bc476566d5ca9e48a4dc18100ff791f359
Nikolai Selivanov <kekchpekk@gmail.com>
2023-08-17 11:35:06 +0300

    Add promise extensions

--- AsyncReactAwait/Promises/Extensions/PromiseExtensions.cs
+++ AsyncReactAwait/Promises/Extensions/PromiseExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace AsyncReactAwait.Promises.Extensions
 {
     /// <summary>
@@ -15,6 +17,43 @@ namespace AsyncReactAwait.Promises.Extensions
         {
             var outcome = new ControllablePromise();
             promise.OnSuccess(_ => outcome.Success());
+            promise.OnFail(e => outcome.Fail(e));
+            return outcome;
+        }
+
+        /// <summary>
+        /// Wraps a non-result to a promise and evaluate it with a result.
+        /// </summary>
+        /// <param name="promise">The promise to wrap.</param>
+        /// <param name="resultGetter">The result getter, that will be called, when promise will completed.</param>
+        /// <typeparam name="T">The return type of wrapped promise.</typeparam>
+        /// <returns>The promise with a result.</returns>
+        public static IPromise<T> Wrap<T>(this IPromise promise, Func<T> resultGetter)
+        {
+            if (promise == null) throw new ArgumentNullException(nameof(promise));
+            if (resultGetter == null) throw new ArgumentNullException(nameof(resultGetter));
+            
+            var outcome = new ControllablePromise<T>();
+            promise.OnSuccess(() => outcome.Success(resultGetter.Invoke()));
+            promise.OnFail(e => outcome.Fail(e));
+            return outcome;
+        }
+
+        /// <summary>
+        /// Converts a promise value to other one.
+        /// </summary>
+        /// <param name="promise">The promise to convert.</param>
+        /// <param name="converter">The converter for source promise result.</param>
+        /// <typeparam name="TSource">The type of a promise result.</typeparam>
+        /// <typeparam name="TRes">The type of conversion result.</typeparam>
+        /// <returns></returns>
+        public static IPromise<TRes> Convert<TSource, TRes>(this IPromise<TSource> promise, Func<TSource, TRes> converter)
+        {
+            if (promise == null) throw new ArgumentNullException(nameof(promise));
+            if (converter == null) throw new ArgumentNullException(nameof(converter));
+            
+            var outcome = new ControllablePromise<TRes>();
+            promise.OnSuccess(x => outcome.Success(converter.Invoke(x)));
             promise.OnFail(e => outcome.Fail(e));
             return outcome;
         }
